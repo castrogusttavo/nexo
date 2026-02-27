31375d8435d8726a34d2f2a16076b27b5f5ae2a0
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2026-02-27 17:28:05 -0300

    feat: add invite-user email route

---
+++ app/api/send/invite-user/route.ts
@@ -0,0 +1,32 @@
+import { Resend } from 'resend'
+import InviteUserEmail from '@/components/emails/invite-user'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+const baseUrl = 'http://localhost:3000'
+
+export async function POST(request: Request) {
+  try {
+    const { email } = await request.json()
+
+    const { data } = await resend.emails.send({
+      from: 'nexo <suporte@coodee.dev>',
+      to: [email],
+      subject: 'Received your invitation',
+      react: InviteUserEmail({
+        username: 'alanturing',
+        userImage: `${baseUrl}/static/vercel-user.png`,
+        invitedByUsername: 'Alan',
+        invitedByEmail: 'alan.turing@example.com',
+        teamName: 'Enigma',
+        teamImage: `${baseUrl}/static/vercel-team.png`,
+        inviteLink: 'https://vercel.com',
+        inviteFromIp: '204.13.186.218',
+        inviteFromLocation: 'São Paulo, Brazil',
+      }),
+    })
+
+    return Response.json(data)
+  } catch (error) {
+    return Response.json({ error: String(error) }, { status: 500 })
+  }
+}
